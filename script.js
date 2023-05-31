667ce67e464bed827ea9668bae2325501c0366b2
Mario Valdez <valdezmario6@gmail.com>
2023-05-31 17:52:49 -0600

    Javascript added

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let listaTareas = [];
+
+const objTarea = {
+id: '',
+nombre: '',
+puesto: ''
+
+}
+
+let editando = false;
+
+const formulario = document.querySelector('#formulario');
+const nombreInput = document.querySelector('#nombre');
+const puestoInput = document.querySelector('#puesto');
+const btnAgregar = document.querySelector('#btnAgregar');
+
+formulario.addEventListener('submit', validarFormulario);
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    if(nombreInput.value === '' || puestoInput.value === '') {
+        alert('Todos los campos son obligatorios.')
+        return;
+    }
+}
 
